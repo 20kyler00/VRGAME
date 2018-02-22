99031023f772156afa20d317f24edd2f2c7bc9c3
Damon Fuller <18.damon.fuller@ksd.org>
2018-02-22 13:47:51 -0800

    Finishing up Little Joe. DRF

---
+++ Assets/ASSETS/DamonsStuff/Scripts/Bosses/LittleJoe/Bouncing.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncing : MonoBehaviour {
+    public float moveTimer = 0.7f;
+    public float spinAttackCountDown = 30;
+    public float spinDuration = 30;
+    public GameObject middle;
+    public GameObject[] locations;
+    public float chaseSpeed = 5;
+    public float howClose = 3;
+    public bool isSpinning;
+    
+
+    private GameObject target;
+    private float timer;
+    private float spinTimer;
+    private float spinningTimer;
+    
+	// Use this for initialization
+	void Start () {
+        target = locations[0];
+        isSpinning = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spinTimer += Time.deltaTime;
+        timer += Time.deltaTime;
+
+        if (spinTimer >= spinAttackCountDown)
+        {
+            spinningTimer += Time.deltaTime;
+            target = middle;
+            Vector3 tempPos = target.transform.position;
+            Vector3 howClose = new Vector3(tempPos.x - transform.position.x, tempPos.y - transform.position.y, tempPos.z - transform.position.z);
+            float closeness = howClose.magnitude;
+            if (closeness <= 3)
+            {
+                isSpinning = true;
+                transform.Rotate(new Vector3(0, 0, 1000 * Time.deltaTime));
+                if (spinningTimer >= spinDuration)
+                {
+                    isSpinning = false;
+                    spinningTimer = 0;
+                    spinTimer = 0;
+                    timer = 0;
+                    NewTarget();
+                }
+            }
+        }
+        else if (timer >= moveTimer)
+        {
+            NewTarget();
+            timer = 0;
+        }
+        Vector3 position = target.transform.position;
+        Vector3 chaseDirection = new Vector3(position.x - transform.position.x, position.y - transform.position.y, position.z - transform.position.z);
+        GetComponent<Rigidbody>().velocity = chaseDirection * chaseSpeed;
+	}
+
+    void NewTarget()
+    {
+        int indexLoc;
+        indexLoc = Random.Range(0, locations.Length);
+        target = locations[indexLoc];
+    }
+}
---
+++ Assets/ASSETS/DamonsStuff/Scripts/Bosses/LittleJoe/FireAtWill.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireAtWill : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public GameObject head;
+    public float bulletSpeed = 5;
+    public float shootFrequency = 30;
+    private float shootTimer = 0;
+    private float shootOG;
+    // Use this for initialization
+    void Start () {
+        shootOG = shootFrequency;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        shootTimer += Time.deltaTime;
+        bool isSpinning = head.GetComponent<Bouncing>().isSpinning;
+        if (isSpinning)
+        {
+            shootFrequency = 0.7f;
+        }
+        else
+        {
+            shootFrequency = shootOG;
+        }
+        if (shootTimer >= shootFrequency)
+        {
+            Vector3 shootDirection = transform.forward;//new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            GameObject Bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            Bullet.GetComponent<Rigidbody>().velocity = shootDirection * bulletSpeed;
+            shootTimer = 0;
+        }
+	}
+}
