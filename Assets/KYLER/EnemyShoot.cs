1ac3fc8745ebd51f099586b5fdfcf67f42afe7f3
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-02-28 12:05:08 -0800

    hi

---
+++ Assets/KYLER/EnemyShoot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShoot : MonoBehaviour {
+	public GameObject prefab;
+	public float bulletspeed = 10f;
+	float totaltime = 0f;
+	public float shootdelay = 3.0f;
+	public float Engagedistance = 10.0f;
+	public GameObject target;
+	int clip = 30;
+	float reload = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		totaltime += Time.deltaTime;
+
+		Vector2 shootd = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y);
+		if (shootd.magnitude <= Engagedistance && totaltime >= shootdelay && clip > 0) {
+			shootd.Normalize ();
+			Vector3 spawnPosition = transform.position;
+			spawnPosition.x += shootd.x * .4f;
+			spawnPosition.y += shootd.y * .4f;
+			GameObject bullet = (GameObject)Instantiate (prefab, spawnPosition, Quaternion.identity);
+			//apply velocity
+			bullet.GetComponent<Rigidbody2D> ().velocity = shootd * bulletspeed;
+			Destroy (bullet, 1.0f);
+			totaltime = 0;
+			clip--;
+		}
+		else if (clip == 0) {
+			reload += Time.deltaTime;
+			if (reload >= 4) {
+				clip = 30;
+
+			}
+	}
+}
+}
---
+++ Assets/KYLER/time.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class time : MonoBehaviour {
+    bool is_slow = false;
+    float time_countdown = 5;
+    public float time_speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(is_slow)
+        {
+            time_countdown -= Time.deltaTime;
+        }
+        else if(time_countdown < 5)
+        {
+            time_countdown += Time.deltaTime;
+        }
+        if(time_countdown < 0)
+        {
+            is_slow = false;
+            Time.timeScale = 1;
+        }
+        if (OVRInput.Get(OVRInput.Button.One) || Input.GetKeyDown(KeyCode.Q))
+        {
+            is_slow = true;
+            Time.timeScale = time_speed;
+        }
+	}
+    
+}
