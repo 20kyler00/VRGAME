c08e1896c2d5eb253d41f6d77315c61c0da000d0
Talia Avery <18.talia.avery@ksd.org>
2018-02-28 12:26:14 -0800

    UI stuff. TA

---
+++ Assets/Tyler/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+	public void PlayEasy ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void PlayNormal ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 2);
+	}
+
+	public void PlayHard ()
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 3);
+	}
+
+	public void Quit ()
+	{
+		Application.Quit ();
+	}
+}
