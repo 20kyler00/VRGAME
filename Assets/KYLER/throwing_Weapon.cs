853140fe9b13a84ca19cbd6bcbae0c8909644dcb
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-02-28 13:04:47 -0800

    hi

---
+++ Assets/KYLER/throwing_Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwing_Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.magnitude - gameObject.transform.parent.transform.position.magnitude <5)
+        {
+            gameObject.transform.SetParent(null);
+        }
+	}
+}
