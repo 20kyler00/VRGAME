3f314179237c8f0fc03052cc353c44e8efebfd30
Kyler Zimmerman <18.kyler.zimmerman@ksd.org>
2018-03-12 11:59:02 -0700

    lololololololololololoololoololol

---
+++ Assets/KYLER/Grenade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+    public GameObject Lid;
+    public bool pinpulled = false;
+    public float timer = 0;
+    bool exploded = false;
+    bool is_already_dead;
+    public AudioSource Pin;
+    public AudioSource Explosion;
+    GameObject player;
+    private void Update()
+    {
+        if (Lid.gameObject.transform.position.magnitude - gameObject.transform.position.magnitude > 5 && !pinpulled)
+        {
+            pinPulled();
+        }
+        if (pinpulled)
+        {
+            timer += Time.deltaTime;
+        }
+        if(timer >= 5&& !exploded)
+        {
+            Explosion.Play();
+            GetComponentInChildren<ParticleSystem>().Play();
+            exploded = true;
+        }
+    }
+    public void pinPulled()
+    {
+        
+        Pin.Play();
+        pinpulled = true;
+        
+    }
+
+}
---
+++ Assets/KYLER/lightChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightChange : MonoBehaviour {
+    public GameObject light;
+    public Material material;
+    // Use this for initialization
+    
+    public void LightChange()
+    {
+        light.transform.GetComponent<MeshRenderer>().material = material;
+
+    }
+}
