28658dbe281db5ae7df75b0fb6210f275aa2ea8d
Damon Fuller <18.damon.fuller@ksd.org>
2018-03-21 12:49:13 -0700

    Adding things to Build settings. DRF

---
+++ Assets/DamonsStuff/Scripts/General/UpdateBossName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateBossName : MonoBehaviour {
+    public string name;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
