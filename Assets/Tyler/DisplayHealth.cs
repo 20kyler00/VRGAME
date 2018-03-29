4f41f98f8d50d4ebc107f31972d6d29a6bc8cbf5
Talia Avery <18.talia.avery@ksd.org>
2018-03-29 13:34:57 -0700

    Health

--- Assets/Tyler/DisplayHealth.cs
+++ Assets/Tyler/DisplayHealth.cs
@@ -5,18 +5,17 @@ using UnityEngine.UI;
 
 public class DisplayHealth : MonoBehaviour 
 {
-	public GameObject player;
 	int health;
 	int maxHealth;
 
 	// Use this for initialization
 	void Start () {
-		maxHealth = player.GetComponent<Health> ().getHealth();
+		maxHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health> ().getHealth();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		health = player.GetComponent<Health> ().getHealth();
+		health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health> ().getHealth();
 		gameObject.GetComponent<Text> ().text = "" + health + " / " + maxHealth;
 	}
 }
