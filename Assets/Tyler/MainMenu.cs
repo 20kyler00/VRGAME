a7907f90b88927f5ad584ce36efe7ac867991332
Talia Avery <18.talia.avery@ksd.org>
2018-03-30 13:15:40 -0700

    Minor

--- Assets/Tyler/MainMenu.cs
+++ Assets/Tyler/MainMenu.cs
@@ -25,7 +25,7 @@ public class MainMenu : MonoBehaviour
 
 	public void ReturnMain ()
 	{
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex - 1);
+		SceneManager.LoadScene ("MainMenu");
 	}
 	public void Quit ()
 	{
